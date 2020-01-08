48c572bc849ca4f8b31f6220daf235534a471450
kerwin <zhang121923@sina.com>
2020-01-08 18:23:27 +0800

    重新定义小作品

--- app/js/index.js
+++ app/js/index.js
@@ -1,17 +1,42 @@
-var app = angular.module('myApp',[]);
-app.controller('myCtrl',function($scope){
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
     //初始状态
-    $scope.state ={
-        isActiveAbout:false,
-        isActiveIntro:false
-    },
+    $scope.state = {
+        isActiveAbout: false,
+        isActiveIntro: false
+    };
+
+    // 菜单项
+    $scope.menuList = [
+        {
+            title: 'Jack zhang`s Blog',
+            content: '个人博客,写写记记,做一个进击的coder',
+            href: 'http://zhang121923.coding.me/'
+        }, {
+            title: '爱生活，爱音乐',
+            content: '小小的那么展示一下我才艺，哈哈',
+            href: ''
+        }, {
+            title: '新的旅途',
+            content: 'You can either travel or read,but either your body or soul must be on the way。这里我选择身体在路上，鄙人不会拍照，仅仅作为留念',
+            href: './app/html/travel/index.html'
+        }, {
+            title: '随笔',
+            content: '小时候听大人说写日记是为以后怀念过去的',
+            href: ''
+        }, {
+            title: '小作品',
+            content: '有时并不知道自己这么能创造',
+            href: './app/html/production/index.html'
+        },
+    ];
 
     //事件
     $scope.events = {
-        show:function (type){
-            if(type == 'isActiveAbout'){
+        show: function (type) {
+            if (type == 'isActiveAbout') {
                 $scope.state.isActiveIntro = false;
-            }else{
+            } else {
                 $scope.state.isActiveAbout = false;
             }
             $scope.state[type] = !$scope.state[type];
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
 var gulp = require("gulp-help")(require("gulp"))
 var src = require('require-dir');
+var browserSync = require('browser-sync').create();
 
 var dir = src("./task");
 
-gulp.task('default','进入所有gulp任务',["index","production","travel"],function(){
+gulp.task('server', '进入所有gulp任务', ["index", "production", "travel"], function () {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "./",
+        },
+        ghostMode: false
+    });
+    gulp.watch(['./app/**', './sass/**', './index.html']).on('change', browserSync.reload);
+});
+
+gulp.task('index', ["gulp_index"], function () {
 
 });
 
-gulp.task('index',["gulp_index"],function(){
+gulp.task('production', ["gulp-production"], function () {
 
 });
 
-gulp.task('production',["gulp-production"],function(){
-
-});
-
-gulp.task('travel',["gulp-travel"],function(){
+gulp.task('travel', ["gulp-travel"], function () {
 
 });
