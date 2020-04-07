44acbf42e67fa3593557922f02558d5554f15b06
kerwin <zhang121923@sina.com>
2020-04-07 15:36:14 +0800

    增加时间轴节点

--- app/js/production/index.js
+++ app/js/production/index.js
@@ -7,22 +7,52 @@
         // 时间轴节点数据
     var timeNode = [
             {
-                time: '2016-08-01',
+                time: '2016-08',
                 name: '找月亮小游戏',
                 desc: '之前自己用Java Swing做过一版，现在用js重做一个页面版',
                 url: './boxGame.html'
             },
             {
-                time: '2016-09-01',
+                time: '2016-09',
                 name: '图片轮播',
                 desc: '造一个图片轮播的轮子',
                 url: 'http://zhang121923.coding.me/picture-carousel/'
             },
             {
-                time: '2017-02-01',
+                time: '2017-02',
                 name: 'react+kendo UI+echarts',
                 desc: '初次尝试react',
                 url: '../../../childPage/myFirstReact/index.html'
+            },
+            {
+                time: '2017-09',
+                name: 'my home',
+                desc: '搭建一个自己的静态网站',
+                url: 'https://zhang121923.github.io/my-home/'
+            },
+            {
+                time: '2018-01',
+                name: 'hexo',
+                desc: '搭建一个自己的blog',
+                url: 'https://zhang121923.github.io/'
+            },
+            {
+                time: '2019-02',
+                name: 'component-build',
+                desc: 'Angular构建一个特殊UI库',
+                url: 'https://github.com/zhang121923/component-build'
+            },
+            {
+                time: '2019-11',
+                name: 'electron-TODO',
+                desc: 'electron做了个仿window 10的To Do桌面程序',
+                url: 'https://github.com/zhang121923/electron-TODO'
+            },
+            {
+                time: '2020-03',
+                name: 'uni-clock',
+                desc: 'uni-app做一个仿华为闹钟应用',
+                url: 'https://github.com/zhang121923/uni-clock'
             }
         ];
 
