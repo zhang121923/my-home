8e2512b127be968887e2465bd199b59cd277c394
zhanglei <zhang121923@sina.com>
2020-04-14 17:19:26 +0800

    首页增加响应式展示

--- app/js/index.js
+++ app/js/index.js
@@ -3,15 +3,24 @@ app.controller('myCtrl', function ($scope) {
     //初始状态
     $scope.state = {
         isActiveAbout: false,
-        isActiveIntro: false
+        isActiveIntro: false,
+        isShowDesc: false
     };
+
+    // temp
+    var screenWidth = document.documentElement.clientWidth;
+    if (screenWidth < 980) {
+        document.querySelector('.header-visible').style.display = 'none';
+    } else {
+        document.querySelector('.header-visible').style.display = 'block';
+    }
 
     // 菜单项
     $scope.menuList = [
         {
             title: 'hope`s Blog',
             content: '个人博客,写写记记,做一个进击的coder',
-            href: 'https://zhang121923.github.io/'
+            href: 'http://www.xiaodoudou.xin'
         }, {
             title: '爱生活，爱音乐',
             content: '小小的那么展示一下我才艺，哈哈',
@@ -40,6 +49,47 @@ app.controller('myCtrl', function ($scope) {
                 $scope.state.isActiveAbout = false;
             }
             $scope.state[type] = !$scope.state[type];
+        },
+        /**
+         * 菜单切换逻辑
+         */
+        toggleDesc: function () {
+            // 切换介绍菜单显示隐藏
+            var headerEle = angular.element(document.querySelector('.header-visible'))[0];
+            var display = headerEle.style.display;
+            headerEle.style.display = display === 'none' ? 'block' : 'none';
+
+            // 显示时默认显示关于项， 关闭是触发两项隐藏
+            if (display === 'none') {
+                $scope.state.isActiveAbout = true;
+            } else {
+                $scope.state.isActiveAbout = false;
+                $scope.state.isActiveIntro = false;
+            }
+
+            // 切换按钮显示
+            $scope.state.isShowDesc = !$scope.state.isShowDesc;
+        },
+        /**
+         * 下页逻辑
+         * @param index
+         */
+        nextPage: function (index, e) {
+            e.preventDefault();
+            // 页码
+            var pageNum = index = index === 4 ? 0 : index + 1;
+            // 获取将要展示的页面element
+            var nextIndex = pageNum + 1;
+            var next = document.querySelector('.kind-item-' + nextIndex);
+            // 所有的页集合
+            var pages = document.querySelectorAll('.page-item');
+            Array.prototype.forEach.call(pages, function (item, i) {
+                if(i === index) {
+                    item.style.zIndex = '5';
+                }else {
+                    item.style.zIndex = '0';
+                }
+            });
         }
     }
 });
